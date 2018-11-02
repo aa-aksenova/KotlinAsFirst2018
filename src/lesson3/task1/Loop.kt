55844a66ee9ea31ed570abfc03b227f6121c4af3
Аделина <atz2@mail.ru>
2018-11-02 16:20:48 +0300

    lesson 3 исправлен

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -2,6 +2,8 @@
 
 package lesson3.task1
 
+import kotlin.math.ceil
+import kotlin.math.floor
 import kotlin.math.sqrt
 
 /**
@@ -99,21 +101,23 @@ fun fib(n: Int): Int {
     }
 }
 
-/**
- * Простая
- *
- * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
- * минимальное число k, которое делится и на m и на n без остатка
- */
-fun lcm(m: Int, n: Int): Int {
+fun nod(m: Int, n: Int): Int {
     var a = m
     var b = n
     while (a != b) {
         if (a > b) a -= b
         else b -= a
     }
-    return m * n / a
+    return a
 }
+
+/**
+ * Простая
+ *
+ * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
+ * минимальное число k, которое делится и на m и на n без остатка
+ */
+fun lcm(m: Int, n: Int): Int = m * n / nod(m, n)
 
 /**
  * Простая
@@ -144,15 +148,7 @@ fun maxDivisor(n: Int): Int =
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean {
-    var a = m
-    var b = n
-    while (a != b) {
-        if (a >= b) a -= b
-        else b -= a
-    }
-    return a == 1
-}
+fun isCoPrime(m: Int, n: Int): Boolean = nod(m, n) == 1
 
 /**
  * Простая
@@ -161,12 +157,8 @@ fun isCoPrime(m: Int, n: Int): Boolean {
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean {
-    for (i in 0..sqrt(n.toDouble()).toInt()) {
-        if (i * i in m..n) return true
-    }
-    return false
-}
+fun squareBetweenExists(m: Int, n: Int): Boolean =
+        floor(sqrt(n.toDouble())) >= ceil(sqrt(m.toDouble()))
 
 /**
  * Средняя
